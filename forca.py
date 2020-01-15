6332fe535995d36661924d2693842f8ed1643d6d
rogerio-stubs <rostubs17@gmail.com>
2020-01-14 23:59:14 -0300

    Aula 2

---
+++ forca.py
@@ -0,0 +1,17 @@
+def jogar():
+    print('*************************************')
+    print('*****Bem vindo ao jogo da Forca!*****')
+    print('*************************************')
+
+    palavra_secreta = 'banana'
+
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+        print('Jogando...')
+
+    print('Fim de jogo')
+
+if (__name__ == '__main__'):
+    jogar()
